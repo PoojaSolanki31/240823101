073aaa7c3d198ead1c77b5a661721a9fc66c99a9
PoojaSolanki31 <poojasolanki3183@gmail.com>
2025-07-14 11:44:33 +0530

    programs

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-// console.log("hello world")
+ console.log("hello world")
 
-// // program-1
+//  program-1
 
-// const name= "Pooja"
-// console.log(name)
+ const name= "Pooja"
+ console.log(name)
 
-// const b=4
-// if (b%2==0)
-// {
-//     console.log("number is even")
-// }
-// else{
-//     console.log("number is odd")
-// }
-// console.log("how are you?")
+ const b=4
+if (b%2==0)
+{
+    console.log("number is even")
+}
+else{
+    console.log("number is odd")
+}
+console.log("how are you?")
 
-// // program-2
+// program-2
 
-// var num1=5
-// var num2=9
+var num1=5
+var num2=9
 
-// if(num1 >= num2)
-// {
-//     console.log("num1 is big")
-// }
-// else{
-//     console.log("num2 is big")
-// }
+if(num1 >= num2)
+{
+    console.log("num1 is big")
+}
+else{
+    console.log("num2 is big")
+}
 
-// // program-3
-// var year=2020
-// if(year%4==0)
-// {
-//     console.log("leap year")
-// }
-// else{
-//     console.log("not leap year")
-// }
+//  program-3
+var year=2020
+if(year%4==0)
+{
+    console.log("leap year")
+}
+else{
+    console.log("not leap year")
+}
 
 // program-4
-// var num = 5
-// var sum = 0
-// for (var i=1;i<=num;i++)
-// {
-//     sum+=i
-// }
-// console.log("sum of num is:",sum)
+var num = 5
+var sum = 0
+for (var i=1;i<=num;i++)
+{
+    sum+=i
+}
+console.log("sum of num is:",sum)
 
 // program-5
 var num = 4
@@ -55,3 +55,5 @@ for (var i=num;i>0;i--)
     fact*=i
 }
 console.log("factoril of num is:",fact)
+
+// program-6
