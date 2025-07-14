d8c0c1aab70affc56e5c65d2f5c7444a8c3e569a
PoojaSolanki31 <poojasolanki3183@gmail.com>
2025-07-14 11:22:37 +0530

    programs

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ else{
     console.log("number is odd")
 }
 console.log("how are you?")
+
+var num1=10
+var num2=20
+
+if(num1 >= num2)
+{
+    console.log("num1 is big")
+}
+else{
+    console.log("num2 is big")
+}
