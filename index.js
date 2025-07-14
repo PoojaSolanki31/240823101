679fe1ecb6605b812f232151f4919e995b7941e8
PoojaSolanki31 <poojasolanki3183@gmail.com>
2025-07-14 11:34:04 +0530

    programs

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 console.log("hello world")
 
-const name= "Pooja"
-console.log(name)
+// // program-1
 
-const b=4
-if (b%2==0)
+// const name= "Pooja"
+// console.log(name)
+
+// const b=4
+// if (b%2==0)
+// {
+//     console.log("number is even")
+// }
+// else{
+//     console.log("number is odd")
+// }
+// console.log("how are you?")
+
+// // program-2
+
+// var num1=5
+// var num2=9
+
+// if(num1 >= num2)
+// {
+//     console.log("num1 is big")
+// }
+// else{
+//     console.log("num2 is big")
+// }
+
+// // program-3
+// var year=2020
+// if(year%4==0)
+// {
+//     console.log("leap year")
+// }
+// else{
+//     console.log("not leap year")
+// }
+
+// program-4
+var num = 5
+var sum = 0
+for (var i=1;i<=num;i++)
 {
-    console.log("number is even")
+    sum+=i
 }
-else{
-    console.log("number is odd")
-}
-console.log("how are you?")
-
-var num1=10
-var num2=20
-
-if(num1 >= num2)
-{
-    console.log("num1 is big")
-}
-else{
-    console.log("num2 is big")
-}
+console.log("sum of num is:",sum)
