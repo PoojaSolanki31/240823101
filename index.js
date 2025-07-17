11c9247ab59586ccd89db0eec466753b275a7602
PoojaSolanki31 <poojasolanki3183@gmail.com>
2025-07-17 08:42:27 +0530

    program

--- index.js
+++ index.js
@@ -1,59 +1,9 @@
- console.log("hello world")
+// using require
 
-//  program-1
+// const add = require("./add.js")
+// console.log(add(11,22));
+// console.log(add(22,10));
 
- const name= "Pooja"
- console.log(name)
-
- const b=4
-if (b%2==0)
-{
-    console.log("number is even")
-}
-else{
-    console.log("number is odd")
-}
-console.log("how are you?")
-
-// program-2
-
-var num1=5
-var num2=9
-
-if(num1 >= num2)
-{
-    console.log("num1 is big")
-}
-else{
-    console.log("num2 is big")
-}
-
-//  program-3
-var year=2020
-if(year%4==0)
-{
-    console.log("leap year")
-}
-else{
-    console.log("not leap year")
-}
-
-// program-4
-var num = 5
-var sum = 0
-for (var i=1;i<=num;i++)
-{
-    sum+=i
-}
-console.log("sum of num is:",sum)
-
-// program-5
-var num = 4
-var fact = 1
-for (var i=num;i>0;i--)
-{
-    fact*=i
-}
-console.log("factoril of num is:",fact)
-
-// program-6
+// using import and export
+import add from "./add.js"
+console.log(add(22,10));
